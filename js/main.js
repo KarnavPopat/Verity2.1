42b67ca18bcb706add39c6007e589f3317602098
Karnav Popat <karnavpopat@gmail.com>
2020-08-12 23:36:54 +0530

    optimization

--- js/main.js
+++ js/main.js
@@ -84,6 +84,7 @@ AOS.init({
 	};
 	mobileMenuOutsideClick();
 
+	// dropdown menu in the side navbar
 	var dropDown = function () {
 
 		$('.dropbtn').on('click', function(event) {
@@ -234,3 +235,34 @@ function search_article() {
 		y.style.display="inline-block !important";
 	}
 } 
+
+// sleep for a set amount of milliseconds
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// display a count from value to value
+async function animateValue(id, start, end, duration) {
+	if(id == "articles") {
+		await sleep(1800);
+	}
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById(id);
+    var timer = setInterval(function() {
+        current += increment;
+        innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+animateValue("writers", 0, 100, 1800);
+animateValue("articles", 0, 125, 1800);
+
+// popup page before user leaves
+/*window.onbeforeunload = function(){
+  return 'Are you sure you want to leave? This is a test message.';
+};*/
