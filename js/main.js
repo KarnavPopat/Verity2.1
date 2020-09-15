ed75ce7ea3c8ca0edff4bc1244798b3ec1caab4f
Karnav Popat <karnavpopat@gmail.com>
2020-09-15 22:24:23 +0530

    overhaul

--- js/main.js
+++ js/main.js
@@ -241,27 +241,6 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// display a count from value to value
-async function animateValue(id, start, end, duration) {
-	if(id == "articles") {
-		await sleep(1800);
-	}
-    var range = end - start;
-    var current = start;
-    var increment = end > start? 1 : -1;
-    var stepTime = Math.abs(Math.floor(duration / range));
-    var obj = document.getElementById(id);
-    var timer = setInterval(function() {
-        current += increment;
-        innerHTML = current;
-        if (current == end) {
-            clearInterval(timer);
-        }
-    }, stepTime);
-}
-animateValue("writers", 0, 100, 1800);
-animateValue("articles", 0, 125, 1800);
-
 // popup page before user leaves
 /*window.onbeforeunload = function(){
   return 'Are you sure you want to leave? This is a test message.';
